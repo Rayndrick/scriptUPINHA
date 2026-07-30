76693f2737989d20a22a3192c14814a1b2412abb
Rayndrick <132782407+Rayndrick@users.noreply.github.com>
2026-07-30 11:24:47 -0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,9 @@
-// Loader do CELK Helper
+(async () => {
+
+    const url = "https://raw.githubusercontent.com/Rayndrick/scriptUPINHA/main/core.js";
+
+    const codigo = await fetch(url + "?v=" + Date.now()).then(r => r.text());
+
+    new Function(codigo)();
+
+})();
