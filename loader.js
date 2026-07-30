cabe5f69c635f27a4d5d6e2fe5fdd28467c57533
Rayndrick <132782407+Rayndrick@users.noreply.github.com>
2026-07-30 11:48:33 -0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,9 +1,5 @@
 (async () => {
 
-    const url = "https://raw.githubusercontent.com/Rayndrick/scriptUPINHA/main/core.js";
-
-    const codigo = await fetch(url + "?v=" + Date.now()).then(r => r.text());
-
-    new Function(codigo)();
+    alert("Loader executado!");
 
 })();
